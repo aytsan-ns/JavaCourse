b7fc81ceafaef1d9f4d50856e2368ba4dc75168a
aytsan-ns <nastyasmirnova40@gmail.com>
2024-12-02 21:20:30 +0300

    решение практики 'Базовые типы данных и литералы'

---
+++ Task2_8.java
@@ -0,0 +1,27 @@
+interface Animal {
+    void sound();
+}
+
+class Dog implements Animal {
+    @Override
+    public void sound() {
+        System.out.println("Woof!");
+    }
+}
+
+class Cat implements Animal {
+    @Override
+    public void sound() {
+        System.out.println("Meow!");
+    }
+}
+public class Task2_8 {
+    public static void main(String[] args) {
+
+        var myDog = new Dog();  // Dog
+        var myCat = new Cat();  // Cat
+
+        myDog.sound();
+        myCat.sound();
+    }
+}
