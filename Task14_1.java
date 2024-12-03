24f8d1d36c0ac6b449d447f3e16b658798a34908
aytsan-ns <nastyasmirnova40@gmail.com>
2024-12-03 21:18:19 +0300

    решение практики 'Методы default и static'

---
+++ Task14_1.java
@@ -0,0 +1,27 @@
+interface myInterface {
+    default void defaultMethod() {
+        System.out.println("Default method");
+    }
+    static void staticMethod() {
+        System.out.println("Static method");
+    }
+}
+
+public class Task14_1 {
+    static class MyClass implements myInterface {
+        // можно переопределить
+        @Override
+        public void defaultMethod() {
+            System.out.println("Overridden default method in the class");
+        }
+    }
+
+    public static void main(String[] args) {
+        MyClass myObject = new MyClass();
+
+        myObject.defaultMethod();
+
+        myInterface.staticMethod();
+    }
+
+}
