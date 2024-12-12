c5457542ed6e3b9c0256603a093246daca3c4a81
aytsan-ns <nastyasmirnova40@gmail.com>
2024-12-12 21:13:28 +0300

    Байтовые и символьные потоки ввода/вывода

--- Task22_4.java
+++ Task22_4.java
@@ -1,2 +1,23 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+class MyResource implements AutoCloseable {
+    public void use() {
+        System.out.println("Ресурс используется");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Ресурс закрыт");
+    }
+}
+
 public class Task22_4 {
+    public static void main(String[] args) {
+        try (MyResource resource = new MyResource()) {
+            resource.use();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
